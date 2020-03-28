7162b1f28f6b025c11bef824078bd468dc76bc86
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-03-28 18:25:12 +0100

    Update ft_strlen.s

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -12,13 +12,13 @@
 
 			global			_ft_strlen
 			section			.text
-
+; Alors notre ft_strlen en C : on met i = -1 ou 0, on incremente tant que str[i] != '\0'
 _ft_strlen:
-			mov				rax, -1 
+			mov				rax, -1 		; ici rax c'est i, et on l'initie à -1
 
-start:
-			inc				rax
-			cmp				byte [rdi + rax], 0
-			jne				start
+start:										; la c'est la boucle, en c on utilise while, en asm on jump
+			inc				rax			; on incremente rax, i++
+			cmp				byte [rdi + rax], 0 	; byte c'est pour checker un seul caractère : est ce que le caractère sur lequel on est == '\0' ?
+			jne				start			; si != '\0', on retourne au début de la boucle
 return:
-			ret
+			ret				; si == '\0', on retourne : ret = rax 
